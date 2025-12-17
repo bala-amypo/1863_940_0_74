15c5c826855d120f30629cbf09549a2e6f9a960f
bala-amypo <bala@amypo.in>
2025-12-17 08:03:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,51 +1,21 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDate;
 
 @Entity
-@Table(name = "student")
-public class Student {
+@Table(name = "users")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
 
-    // Constructors
-    public Student() {
-    }
+    private String password;
 
-    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getters & Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email =email;
-    }
+    private String role;
 }
